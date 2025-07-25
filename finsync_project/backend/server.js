c90e3a619e471fef6256e7ddd87344b4543605e1
Guilherme Silva Machado <guisilvam19@gmail.com>
2025-07-24 23:34:59 -0300

    Add projeto

---
+++ finsync_project/backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch(err => console.error(err));
+
+app.get("/", (req, res) => res.send("API online"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
---
+++ finsync_project/frontend/src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function App() {
+  return <div className="text-center p-10">Finsync Frontend Pronto!</div>;
+}
+export default App;
